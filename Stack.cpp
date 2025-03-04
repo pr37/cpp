1d99ca785e35f69f790682b71d5f17c151551bec
D.T. <61751087+pr37@users.noreply.github.com>
2025-03-04 14:11:28 +0100

    Create Stack.cpp

---
+++ Stack.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <stack>
+
+#define STEK_SIZE 100
+
+using namespace std;
+
+//s.top()
+//s.pop()
+//s.push()
+
+int main() {
+
+    stack <int> stek;
+    for(i=0;i<=STEK_SIZE;i++) {
+        stek.push(i);
+    }
+    while(stek.empty()) {
+        cout << stek.top() << endl;
+        st.pop()
+    }
+}
+
+//100 do 0
+
